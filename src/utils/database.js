e03a3494c3e4665b7ce77242c475e5ebb76faee3
Lizzie Hand <elizabethghand@gmail.com>
2021-08-04 21:16:25 +0100

    first commit

---
+++ src/utils/database.js
@@ -0,0 +1,9 @@
+const { Client } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connection = process.env.PGURL;
+
+const db = new Client(connection);
+
+module.exports = db;
